fe01fb2458e400a9a9adaa836c275d3d4e35fd98
catianri <1939653182@qq.com>
2024-08-16 16:38:30 +0800

    exemple

---
+++ scripts/debug.py
@@ -0,0 +1,12 @@
+import searcher
+from scripts.searcher import Searcher
+
+msg = '../data/output/raw.txt'
+
+sc = Searcher()
+sc.init_searcher(msg)
+
+while True:
+    buf = input('Please Enter your Search Query#')
+    print(sc.search(buf))
+
